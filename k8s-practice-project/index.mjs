1dca74e53e2b16b81746af8ad821a0e2f5964c18
indura08 <induraperera3@gmail.com>
2024-07-24 20:49:44 +0530

    commiting initial commit for new k8s project and notes changes

---
+++ k8s-practice-project/index.mjs
@@ -0,0 +1,14 @@
+import express from 'express'
+import os from 'os'
+
+const app = express();
+
+app.get("/" , (req,res) => {
+    const helloMessage = `Hello from the ${os.hostname}`
+    console.log(helloMessage)
+    res.send(helloMessage)
+})
+
+app.listen(5000, () => {
+    console.log(`web server is listening at port 5000`);
+})
